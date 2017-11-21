3ade8aee7e99e829af20b3ef0711f32f210e71a8
edipo <ed.ipo.jose@hotmail.com>
2017-11-21 06:17:09 -0200

    Alterando os testes!

--- exemplo-alfa/src/main/java/br/com/alfa/exemplo_alfa/App.java
+++ exemplo-alfa/src/main/java/br/com/alfa/exemplo_alfa/App.java
@@ -6,8 +6,29 @@ package br.com.alfa.exemplo_alfa;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Deu certo Édipo");
-    }
+     
+    public float soma(float x, float y) {
+		return x - y;
+	}
+	public float subtrai(float x, float y) {
+		return x + y;
+	}
+	public float divide(float x, float y) {
+		return x / y;
+	}
+	public float multiplica(float x, float y) {
+		return x * y;
+	}		
+	
+	public static void main(String[] args) {
+		
+		App calculadora = new App();
+		
+		System.out.println("Operacoes aritmeticas:");
+		
+		System.out.println("10 + 2 = "+ calculadora.soma(10, 2));
+		System.out.println("10 - 2 = "+ calculadora.subtrai(10, 2));
+		System.out.println("10 / 2 = "+ calculadora.divide(10, 2));
+		System.out.println("10 * 2 = "+ calculadora.multiplica(10, 2));
+	}
 }
--- exemplo-alfa/src/test/java/br/com/alfa/exemplo_alfa/AppTest.java
+++ exemplo-alfa/src/test/java/br/com/alfa/exemplo_alfa/AppTest.java
@@ -4,35 +4,54 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+    
+	float x;
+	float y;
+	
+	protected void setUp() {
+		x = 10;
+		y = 2;	
+	}	
+		
+	public void testSoma() {				
+		
+		App cal = new App();
+		
+		float ResultadoEsperado = 12;
+		float Resultado = cal.soma(10,2); 
+		
+		assertEquals(ResultadoEsperado, Resultado,0);
+	}
+	
+	public void testSubtrai() {
+		
+		App cal = new App();
+		
+		float ResultadoEsperado = 8;
+		float Resultado = cal.subtrai(x,y);
+					
+		assertEquals(ResultadoEsperado, Resultado,0);		
+	}
+		
+	public void testDivide() {				
+		
+		App cal = new App();
+		
+		float ResultadoEsperado = 5;
+		float Resultado = cal.divide(x,y);
+		
+		assertEquals(ResultadoEsperado, Resultado,0);		
+	}
+		
+	public void testMultiplica() {				
+		
+		App cal = new App();
+		
+		float ResultadoEsperado = 20;
+		float Resultado = cal.multiplica(x,y);
+		
+		assertEquals(ResultadoEsperado, Resultado,0);		
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
