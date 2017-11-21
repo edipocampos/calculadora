50a41c62d944082a82070f82c8b82f749042dda6
edipo <ed.ipo.jose@hotmail.com>
2017-11-21 21:15:14 -0200

    Criação do proejto Calculadora (CI/CD)

---
+++ workspace/calculadora/src/main/java/com/calculadora/App.java
@@ -0,0 +1,18 @@
+package com.calculadora;
+
+public class App {
+	
+	public float soma(float x, float y) {
+		return x * y;
+	}
+	public float subtrai(float x, float y) {
+		return x - y;
+	}
+	public float divide(float x, float y) {
+		return x / y;
+	}
+	public float multiplica(float x, float y) {
+		return x * y;
+	}	
+
+}
